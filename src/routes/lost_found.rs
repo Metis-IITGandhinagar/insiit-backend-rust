2be1391aabcadc460d33663bb9b7e72a946cd16a
Advait <advaitandhale87@gmail.com>
2026-07-22 10:31:45 +0530

    lost found done

--- src/routes/lost_found.rs
+++ src/routes/lost_found.rs
@@ -5,21 +5,23 @@ use sqlx::{ query, query_as };
 use time::OffsetDateTime;
 
 use crate::AppState;
-use crate::schemas::lost_found_schemas::{ LostFoundEntry, LostFoundRequest };
+use crate::schemas::lost_found_schemas::{ LostFoundEntry, LostFoundRequest, LostFoundStatus, LostFoundClaim };
 
 
 pub fn get_routes() -> Router<AppState> {
     Router::new()
         .route("/lost-found", get(get_all_lost_found))
-        .route("/lost-found/{id}", get(get_lost_found_by_id))
-        .route("/lost-found", delete(delete_lost_found))
+        .route("/lost-found/:id", get(get_lost_found_by_id))
+        .route("/lost-found/:id", delete(delete_lost_found))
         .route("/lost-found", put(edit_lost_found))
         .route("/lost-found", post(add_lost_found))
+        .route("/lost-found/claim-found", post(claim_found))
+        .route("/lost-found/mark-found", put(mark_found))
 }
 
 async fn get_all_lost_found(State(state): State<AppState>) -> Result<JsonResponse<Vec<LostFoundEntry>>, (StatusCode, String)> {
     match query_as::<_, LostFoundEntry>(
-        "SELECT id, item_name, description, added_on_timestamp, added_by_email, is_found, img_urls FROM lostfoundentries"
+        "SELECT id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls FROM lostfoundentries"
     )
         .fetch_all(&state.pool)
         .await {
@@ -36,7 +38,7 @@ async fn get_all_lost_found(State(state): State<AppState>) -> Result<JsonRespons
 
 async fn get_lost_found_by_id(State(state): State<AppState>, Path(id): Path<i32>) -> Result<JsonResponse<LostFoundEntry>, (StatusCode, String)> {
     match query_as::<_, LostFoundEntry>(
-        "SELECT id, item_name, description, added_on_timestamp, added_by_email, is_found, img_urls FROM lostfoundentries WHERE id = $1"
+        "SELECT id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls FROM lostfoundentries WHERE id = $1"
     )
         .bind(id)
         .fetch_one(&state.pool).await {
@@ -55,7 +57,7 @@ async fn get_lost_found_by_id(State(state): State<AppState>, Path(id): Path<i32>
         }
 }
 
-async fn add_lost_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Json(lost_found_request): Json<LostFoundRequest>) -> Result<JsonResponse<LostFoundRequest>, (StatusCode, String)> {
+async fn add_lost_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Json(lost_found_request): Json<LostFoundRequest>) -> Result<JsonResponse<LostFoundEntry>, (StatusCode, String)> {
     let token = auth_header.token().to_string();
     let user = match state.firebase_token_validator.clone().validate(token).await {
         Ok(user) => {
@@ -82,18 +84,21 @@ async fn add_lost_found(State(state): State<AppState>, TypedHeader(auth_header):
         img_urls.push(url);
     }
 
-    match query(
-        "INSERT INTO lostfoundentries(item_name, description, added_on_timestamp, added_by_email, img_urls) values($1, $2, $3, $4, $5);"
+    match query_as::<_, LostFoundEntry>(
+        "INSERT INTO lostfoundentries(item_name, description, added_on_timestamp, added_by_email, img_urls)
+        VALUES($1, $2, $3, $4, $5)
+        RETURNING id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls;
+        "
     )
         .bind(&lost_found_request.item_name)
         .bind(&lost_found_request.description)
         .bind(timestamp)
         .bind(email)
         .bind(img_urls)
-        .execute(&state.pool).await {
-            Ok(_) => {
+        .fetch_one(&state.pool).await {
+            Ok(new_lost_found_entry) => {
                 log::info!("LostFound: Added item lost_found_entry");
-                Ok(Json(lost_found_request))
+                Ok(Json(new_lost_found_entry))
             },
             Err(e) => {
                 log::error!("LostFound: Error adding lost_found_entry: {e}");
@@ -121,19 +126,22 @@ async fn edit_lost_found(State(state): State<AppState>, TypedHeader(auth_header)
         },
         None => return Err((StatusCode::FORBIDDEN, String::from("Invalid user"))),
     };
-    match query(
-        "UPDATE lostfoundentries set item_name = $1, description = $2, is_found = $3, img_urls = $4 WHERE id = $5 AND added_by_email = $6"
+    match query_as::<_, LostFoundEntry>(
+        "UPDATE lostfoundentries
+        SET item_name = $1, description = $2, img_urls = $3
+        WHERE id = $4 AND added_by_email = $5
+        RETURNING id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls
+        "
     )
         .bind(&lost_found_entry.item_name)
         .bind(&lost_found_entry.description)
-        .bind(&lost_found_entry.is_found)
         .bind(&lost_found_entry.img_urls)
         .bind(&lost_found_entry.id)
         .bind(email)
-        .execute(&state.pool).await {
-            Ok(_) => {
+        .fetch_one(&state.pool).await {
+            Ok(updated_lost_found_entry) => {
                 log::info!("LostFound: Added item lost_found_entry");
-                Ok(Json(lost_found_entry))
+                Ok(Json(updated_lost_found_entry))
             },
             Err(sqlx::error::Error::RowNotFound) => {
                 log::info!("LostFound: didn't find any lost_found_entry from database.");
@@ -146,7 +154,7 @@ async fn edit_lost_found(State(state): State<AppState>, TypedHeader(auth_header)
         }
 }
 
-async fn delete_lost_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Json(lost_found_entry): Json<LostFoundEntry>) -> Result<JsonResponse<LostFoundEntry>, (StatusCode, String)>{
+async fn delete_lost_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Path(id): Path<i32>) -> Result<JsonResponse<()>, (StatusCode, String)>{
     let token = auth_header.token().to_string();
     let user = match state.firebase_token_validator.clone().validate(token).await {
         Ok(user) => {
@@ -168,20 +176,102 @@ async fn delete_lost_found(State(state): State<AppState>, TypedHeader(auth_heade
     match query(
         "DELETE FROM lostfoundentries WHERE id = $1 AND added_by_email = $2"
     )
-        .bind(&lost_found_entry.id)
+        .bind(id)
         .bind(email)
         .execute(&state.pool).await {
-            Ok(_) => {
+            Ok(result) if result.rows_affected() > 0 => {
                 log::info!("LostFound: Delete item lost_found_entry");
-                Ok(Json(lost_found_entry))
+                Ok(Json(()))
             },
-            Err(sqlx::error::Error::RowNotFound) => {
-                log::info!("LostFound: didn't find any lost_found_entry from database.");
-                Err((StatusCode::NOT_FOUND, String::from("Couldn't get lost_found_entry from db")))
+            Ok(_) => {
+                Err((StatusCode::NOT_FOUND, String::from("entry not found")))
             },
             Err(e) => {
                 log::error!("LostFound: Error adding lost_found_entry: {e}");
                 Err((StatusCode::INTERNAL_SERVER_ERROR, String::from("Couldn't delete lost found entry from the database")))
+            }
+        }
+}
+
+async fn mark_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Json(mut lost_found_entry): Json<LostFoundEntry>) -> Result<JsonResponse<LostFoundEntry>, (StatusCode, String)> {
+    let token = auth_header.token().to_string();
+    let user = match state.firebase_token_validator.clone().validate(token).await {
+        Ok(user) => {
+            log::info!("LostFound: Found user for mark_found lost_found_entry");
+            user
+        },
+        Err(e) => {
+            log::error!("LostFound: Couldn't find user for mark_found lost_found_entry");
+            return Err((StatusCode::FORBIDDEN, String::from("Couldn't authenticate user")))
+        }
+    };
+    let email = match user.get("email") {
+        Some(value) => match value.as_str() {
+            Some(email) => email,
+            None => return Err((StatusCode::FORBIDDEN, String::from("Invalid user"))),
+        },
+        None => return Err((StatusCode::FORBIDDEN, String::from("Invalid user"))),
+    };
+    match query_as::<_, LostFoundEntry>(
+        "UPDATE lostfoundentries
+        SET status = 'found'
+        WHERE id = $1 AND added_by_email = $2
+        RETURNING id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls
+        "
+    )
+        .bind(lost_found_entry.id)
+        .bind(email)
+        .fetch_one(&state.pool).await {
+            Ok(updated_lost_found_entry) => {
+                log::info!("LostFound: Marked item found");
+                Ok(Json(updated_lost_found_entry))
+            },
+            Err(e) => {
+                log::info!("LostFound: Couldn't update item");
+                Err((StatusCode::FORBIDDEN, String::from("Couldn't update item as found")))
+            }
+        }
+}
+
+async fn claim_found(State(state): State<AppState>, TypedHeader(auth_header): TypedHeader<Authorization<Bearer>>, Json(mut claim_request): Json<LostFoundClaim>) ->  Result<JsonResponse<LostFoundEntry>, (StatusCode, String)> {
+    let token = auth_header.token().to_string();
+    let user = match state.firebase_token_validator.clone().validate(token).await {
+        Ok(user) => {
+            log::info!("LostFound: Found user for claim_found lost_found_entry");
+            user
+        },
+        Err(e) => {
+            log::error!("LostFound: Couldn't find user for saving lost_found_entry: {e}");
+            return Err((StatusCode::FORBIDDEN, String::from("Couldn't authenticate user")))
+        }
+    };
+    let email = match user.get("email") {
+        Some(value) => match value.as_str() {
+            Some(email) => email,
+            None => return Err((StatusCode::FORBIDDEN, String::from("Invalid user"))),
+        },
+        None => return Err((StatusCode::FORBIDDEN, String::from("Invalid user"))),
+    };
+    claim_request.claimed_by_email = String::from(email);
+    match query_as::<_, LostFoundEntry>(
+        "UPDATE lostfoundentries
+        SET found_claims = COALESCE(found_claims, '[]'::jsonb) || $1::jsonb,
+            status = 'claimed_to_be_found'
+        WHERE id = $2 AND (status = 'claimed_to_be_found' OR status = 'lost')
+        RETURNING id, item_name, description, added_on_timestamp, added_by_email, status, found_claims, img_urls
+        "
+    )
+        .bind(&serde_json::to_value(&claim_request).expect("found_clams is a vec"))
+        .bind(&claim_request.id)
+        .fetch_one(&state.pool).await {
+            Ok(update_lost_found_entry) => Ok(Json(update_lost_found_entry)),
+            Err(sqlx::error::Error::RowNotFound) => {
+                log::info!("didn't find appropriate entry to be updated");
+                Err((StatusCode::NOT_FOUND, String::from("Couldn't add found claim in the database")))
+            }
+            Err(e) => {
+                log::error!("LostFound: Couldn't update found claims: {e}");
+                Err((StatusCode::INTERNAL_SERVER_ERROR, String::from("Couldn't add found claim in the database")))
             }
         }
 }
--- src/schemas/lost_found_schemas.rs
+++ src/schemas/lost_found_schemas.rs
@@ -1,17 +1,21 @@
 use serde::{ Serialize, Deserialize };
-use sqlx::{ FromRow, PgPool, postgres::PgQueryResult, query };
+use sqlx::{ FromRow, PgPool, postgres::PgQueryResult, query, Type };
 use time::OffsetDateTime;
 
+// WARNING / BUG: status has skip_deserializing, but it's an enum without default value. In the
+// routes file, check what value comes when a request is sent.
 #[derive(Serialize, Deserialize, FromRow)]
 pub struct LostFoundEntry {
-    #[serde(skip_deserializing)]
     pub id: i32,
     pub item_name: String,
     pub description: String,
     #[serde(skip_deserializing, default = "OffsetDateTime::now_utc", with = "time::serde::timestamp")]
     pub added_on_timestamp: OffsetDateTime,
     pub added_by_email: String,
-    pub is_found: bool,
+    #[serde(skip_deserializing)]
+    pub status: LostFoundStatus,
+    #[sqlx(json)]
+    pub found_claims: Vec<LostFoundClaim>,
     pub img_urls: Vec<String>
 }
 
@@ -22,7 +26,36 @@ pub struct LostFoundRequest {
     pub base64_images: Vec<String>,
 }
 
+
+#[derive(Serialize, Deserialize, FromRow)]
+pub struct LostFoundClaim {
+    pub id: i32,
+    pub item_name: String,
+    #[serde(skip_deserializing)]
+    pub claimed_by_email: String,
+    pub remarks: String,
+    #[serde(skip_deserializing, default = "OffsetDateTime::now_utc", with = "time::serde::timestamp")]
+    pub claim_timestamp: OffsetDateTime
+}
+
+#[derive(Type, Serialize, Deserialize, Default, Debug, Clone, PartialEq)]
+#[sqlx(type_name = "item_status", rename_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum LostFoundStatus {
+    #[default] Lost, Found, ClaimedToBeFound
+}
+
 pub async fn initialize_table(pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    sqlx::query(
+        "DO $$ BEGIN
+            CREATE TYPE item_status as ENUM('lost', 'found', 'claimed_to_be_found');
+        EXCEPTION
+            WHEN duplicate_object THEN null;
+        END $$;
+        "
+    )
+        .execute(pool)
+        .await?;
     query("
         CREATE TABLE IF NOT EXISTS lostfoundentries (
             id SERIAL PRIMARY KEY,
@@ -30,7 +63,8 @@ pub async fn initialize_table(pool: &PgPool) -> Result<PgQueryResult, sqlx::Erro
             description VARCHAR(1023) NOT NULL,
             added_on_timestamp TIMESTAMPTZ NOT NULL,
             added_by_email VARCHAR(255) NOT NULL,
-            is_found BOOLEAN NOT NULL DEFAULT false,
+            status item_status NOT NULL DEFAULT 'lost',
+            found_claims JSONB NOT NULL DEFAULT '[]',
             img_urls VARCHAR(255)[] NOT NULL DEFAULT '{}'
         );
     ")
